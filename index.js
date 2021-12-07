0173a2ae8e02808ef89d356644a89b5313b8c27a
MariiaUlankina <ulankina.masha@gmail.com>
2021-12-07 18:19:34 +0200

    change js file

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
     function answer(){
 
         while (true){
-            let result = prompt ('Enter words');
-            for (let words = 0; words < 3; words++){
-                words = prompt ('Enter words');
-                result = result+words;
-                console.log(result);
-            }  
+
+            for (let step = 0; step < 2; step++){
+                let result = prompt ('Enter words');
+                console.log(result);  
+            }       
+            
               
             let question = confirm ('Сontinue or not?');
             if (question === false){
